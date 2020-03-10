a1befe8a120cf05c1f91a10e8907b9513ee0fd28
claymccoy <git@claymccoy.com>
2020-03-09 23:15:41 -0500

    added tags to datadog event
    also added unit tests and changed http lib to one that would be used in Echo due to an issue with the plugin bundler that keeps new deps from working

--- event-listener-echo/src/main/kotlin/io/armory/plugin/events/listener/datadog/DataDogEventListenerPlugin.kt
+++ event-listener-echo/src/main/kotlin/io/armory/plugin/events/listener/datadog/DataDogEventListenerPlugin.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 Armory, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.armory.plugin.events.listener.datadog
 
 import com.netflix.spinnaker.echo.api.events.Event
@@ -7,11 +22,12 @@ import org.pf4j.Extension
 import org.pf4j.Plugin
 import org.pf4j.PluginWrapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.github.kittinunf.fuel.Fuel
 
-import com.github.kittinunf.result.success
-import com.github.kittinunf.result.failure
-import java.lang.IllegalStateException
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import org.slf4j.Logger
 
 class DataDogEventListenerPlugin(wrapper: PluginWrapper) : Plugin(wrapper) {
     private val logger = LoggerFactory.getLogger(DataDogEventListenerPlugin::class.java)
@@ -29,40 +45,64 @@ data class DataDogEvent(
         val title: String,
         val text: String,
         val priority: String,
-        val tags: List<String>,
+        val tags: Set<String>,
         val alert_type: String
 )
 
 @Extension
-class DataDogEventListener(val configuration: DataDogEventListenerConfig) : EventListener {
+open class DataDogEventListener(val configuration: DataDogEventListenerConfig) : EventListener {
 
     private val log = LoggerFactory.getLogger(DataDogEventListener::class.java)
 
     private val mapper = jacksonObjectMapper()
 
+    protected open fun getHttpClient() : OkHttpClient {
+        return OkHttpClient()
+    }
+
+    protected open fun getLogger() : Logger {
+        return log
+    }
+
     override fun processEvent(event: Event) {
 
+        val tags = mutableSetOf(
+                "source:${event.details.source}",
+                "eventType:${event.details.type}",
+                "application:${event.details.application}"
+        )
+        val execution = event.content["execution"] as Map<String, Any?>
+        tags.add("executionType:${execution["type"]}")
+        tags.add("executionStatus:${execution["status"]}")
+        tags.add("executionId:${execution["id"]}")
+        execution["name"]?.let {
+            tags.add("pipelineName:$it")
+        }
+        execution["pipelineConfigId"]?.let {
+            tags.add("pipelineConfigId:$it")
+        }
+
         val dataDogEvent = DataDogEvent(
-                "test echo to DataDog",
+                "Spinnaker Event",
                 mapper.writeValueAsString(event),
                 "normal",
-                listOf("environment:test"),
+                tags,
                 "info"
         )
-
         val dataDogEventJson = mapper.writeValueAsString(dataDogEvent)
-        println(dataDogEventJson)
-
         val dataDogUrl = "https://api.datadoghq.com/api/v1/events"
-        val (request, response, result) = Fuel.post(dataDogUrl, listOf("api_key" to configuration.apiKey))
-                .header(mapOf(
-                        "Content-Type" to "application/json"
-                ))
-                .body(dataDogEventJson)
-                .response()
 
-        result.failure {
-            log.error("DataDog event listener failed with response: $response")
+        val body = RequestBody.create(
+                MediaType.parse("application/json"), dataDogEventJson)
+        val request = Request.Builder()
+                .url("$dataDogUrl?api_key=${configuration.apiKey}")
+                .post(body)
+                .build()
+        val call = getHttpClient().newCall(request)
+        val response = call.execute()
+
+        if (!response.isSuccessful) {
+            getLogger().error("DataDog event listener failed with response: ${response.toString()}")
         }
     }
 }
